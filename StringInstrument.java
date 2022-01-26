0f010c81d66d822acbef1f138b41f4e11d97ca1a
Tim Moriarty <timmoriarty@gmail.com>
2022-01-26 14:12:22 -0600

    Initial files

---
+++ StringInstrument.java
@@ -0,0 +1,13 @@
+// TODO: Define a class: StringInstrument that is derived from the Instrument class
+// TODO: Set up inheritance relationship
+public class StringInstrument  {
+   // TODO: Declare private fields: numStrings, numFrets
+
+   // TODO: Define mutator methods - 
+   //      setNumOfStrings(), setNumOfFrets()
+
+   // TODO: Define accessor methods -
+   //      getNumOfStrings(), getNumOfFrets()
+
+}
+
